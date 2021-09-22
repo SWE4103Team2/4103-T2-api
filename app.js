be283641ad8dfedd66946313c5f4a30f2e299236
rgreenan55 <rjgreenan55@gmail.com>
2021-09-22 18:12:22 -0300

    Added CORS. Added example API

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-const port = process.env.PORT || 3001;
-// const x = require('./routes/x.js');
+app.use(cors({
+  origin: '*'
+}))
 
-// app.use('/x', x);
+const port = process.env.PORT || 3001;
+const example = require('./routes/example.js');
+
+app.use('/example', example);
 
 app.listen(port, () => console.log('App listening on port', port));
