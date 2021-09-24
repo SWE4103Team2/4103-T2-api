ded05ce68e9cc86ab8e5c9e152e7be04eb9f38e2
rgreenan55 <rjgreenan55@gmail.com>
2021-09-24 12:51:15 -0300

    Added fileType checks, just need headers from sample files

---
+++ config/fileTypes.js
@@ -0,0 +1,4 @@
+export const invalidData = 0;
+export const courseData = 1;
+export const personData = 2;
+export const transferData = 3;
--- util/fileUtil.js
+++ util/fileUtil.js
@@ -1,3 +1,5 @@
+import { invalidData, courseData, personData, transferData } from "../config/fileTypes";
+
 export const readFile = (file) => {
   const reader = new FileReader();
   let data = null;
@@ -16,4 +18,21 @@ export const splitByNewLine = (string) => {
 
 export const splitByTab = (string) => {
   return string.split('\t');
+}
+
+// Get these from sample files.
+const courseDataHeading = "";
+const personDataHeading = "";
+const transferDataHeading = "";
+
+export const checkFileType = (string) => {
+  if (string === courseDataHeading) {
+    return courseData;
+  } else if (string === personDataHeading) {
+    return personData;
+  } else if (string === transferDataHeading) {
+    return transferData;
+  } else {
+    return invalidData;
+  }
 }
