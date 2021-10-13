12f82b4829a6b3d3876d352dd4bc50f35364bf3f
dunc415 <duncanchet2001@hotmail.com>
2021-10-13 16:55:47 -0300

    query updates

--- routes/students.js
+++ routes/students.js
@@ -10,10 +10,36 @@ router.get('/', async (req, res) => {
       host: 'themis.xn--9xa.network',
       dialect:'mariadb'
     });
-    const [results, metadata] = await sequelize.query("SELECT stuID, name, sDate, program FROM Student");
+    const [results, metadata] = await sequelize.query("SELECT student_ID, Name, Start_Date, Program FROM Student");
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+  }
+});
+router.get('/getStudent', async (req, res) => {
+  try {
+    const studentID = req.query.id
+    const sequelize = new Sequelize('SWE4103T2', 'admin', 'PQzU$5vchmev!T2d%w8yCGosfqg%q#!9hcLN%c', {
+      host: 'themis.xn--9xa.network',
+      dialect:'mariadb'
+    });
+    const [results, metadata] = await sequelize.query("SELECT student_ID, Name, Start_Date, Program FROM Student WHERE student_ID = " + studentID);
+    res.json(results);
   } catch (err) {
     console.error(err);
   }
 });
 
+router.get('/getFiles', async (req, res) => {
+  try {
+    const sequelize = new Sequelize('SWE4103T2', 'admin', 'PQzU$5vchmev!T2d%w8yCGosfqg%q#!9hcLN%c', {
+      host: 'themis.xn--9xa.network',
+      dialect:'mariadb'
+    });
+    const [results, metadata] = await sequelize.query("SELECT fileID FROM Student GROUP BY fileID");
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+  }
+});
 module.exports = router;
