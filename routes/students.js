79acd4970d67eee1706d8f4b580ab9a3497cde33
bjacobsUNB <bjacobs@UNB.ca>
2021-10-20 22:57:34 -0300

    Added query to get Year (rank as a number)
    Added 3 ways to get rank:
        By credit hour
        By exact start date
        By cohort
    
    I had to do raw queries as these are kind of complex queries
    It may be possible to convert them to the Sequelize, but thats for a later date

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const db = require('../models/index');
+const e = require('express');
 
 let router = express.Router();
 
@@ -27,7 +28,6 @@ router.get('/getStudents', async (req, res) => {
       where.Student_ID = req.query.id;
     }
     const fileTimeTable = await db.Student.findAll({ 
-      attributes: ['Student_ID', 'Name', 'Start_Date', 'Program'], 
       where
     });
     const fileList = fileTimeTable.map( row => {
@@ -44,7 +44,7 @@ router.get('/getFiles', async (req, res) => {
     const fileTimeTable = await db.FileTime.findAll({ 
       attributes: ['fileID'], 
       order: [
-        ['fileID', 'DESC']
+        ['uploadTime', 'DESC']
       ]
     });
     const fileList = fileTimeTable.map( row => {
@@ -55,4 +55,37 @@ router.get('/getFiles', async (req, res) => {
     console.error(err);
   }
 });
+
+router.get('/getYear', async (req, res) =>{
+  try {
+    let SQLQuery;
+    if(req.query.type === "0"){         // 0 means by credit hours (40h per year)
+      SQLQuery = "SELECT CEILING(SUM(Credit_Hrs)/40) AS 'Year' FROM Student LEFT JOIN Enrollment ON Student.Student_ID = Enrollment.Student_ID AND Student.fileID = Enrollment.fileID WHERE Student.fileID = '" + req.query.file + "'";
+      if(req.query.id !== ""){
+        SQLQuery += " AND Student.fileID = '" + req.query.id + "'";
+      }
+      SQLQuery += " GROUP BY Student.Student_ID";
+    }
+    else if(req.query.type === "1"){    // 1 means by exact start date
+      SQLQuery = "SELECT CEILING(DATEDIFF(NOW(), Start_Date)/365) AS 'Year' FROM Student WHERE Student.fileID = '" + req.query.file + "'";
+      if(req.query.id !== ""){
+        SQLQuery += " AND Student.fileID = '" + req.query.id + "'";
+      }
+    }
+    else if(req.query.type === "2"){    // 2 means by cohort
+      SQLQuery = "SELECT CEILING(DATEDIFF(NOW(), ADDDATE(Start_Date, -243))/365) AS 'Year' FROM Student WHERE Student.fileID = '" + req.query.file + "'";
+      if(req.query.id !== ""){
+        SQLQuery += " AND Student.fileID = '" + req.query.id + "'";
+      }
+    }
+    if(SQLQuery === undefined){
+      res.json([]);
+      return;
+    }
+    const fileTimeTable = await db.sequelize.query(SQLQuery);
+    res.json(fileTimeTable[0]);
+  } catch (err) {
+    console.error(err);
+  }
+})
 module.exports = router;
