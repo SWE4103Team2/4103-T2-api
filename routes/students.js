6cd02b8b11bf4a74bb625d791f04def0085d380d
bjacobsUNB <bjacobs@UNB.ca>
2021-10-26 18:42:18 -0300

    Bug testing complete, test more if you want

--- routes/students.js
+++ routes/students.js
@@ -91,20 +91,20 @@ router.get('/getYear', async (req, res) =>{
     if(req.query.type === "0"){         // 0 means by credit hours (40h per year)
       SQLQuery = "SELECT CEILING(SUM(Credit_Hrs)/40) AS 'Year' FROM Student LEFT JOIN Enrollment ON Student.Student_ID = Enrollment.Student_ID AND Student.fileID = Enrollment.fileID WHERE Student.fileID = '" + req.query.file + "'";
       if(req.query.id !== ""){
-        SQLQuery += " AND Student.Student_ID = '" + req.query.id + "'";
+        SQLQuery += " AND (Student.Student_ID LIKE '%" + req.query.id + "%' OR Student.Name = '" + req.query.id + "' OR Student.Start_Date = '" + req.query.id + "' OR Student.Program = '" + req.query.id + "')";
       }
       SQLQuery += " GROUP BY Student.Student_ID";
     }
     else if(req.query.type === "1"){    // 1 means by exact start date
       SQLQuery = "SELECT CEILING(DATEDIFF(NOW(), Start_Date)/365) AS 'Year' FROM Student WHERE Student.fileID = '" + req.query.file + "'";
       if(req.query.id !== ""){
-        SQLQuery += " AND Student.Student_ID = '" + req.query.id + "'";
+        SQLQuery += " AND (Student.Student_ID LIKE '%" + req.query.id + "%' OR Student.Name = '" + req.query.id + "' OR Student.Start_Date = '" + req.query.id + "' OR Student.Program = '" + req.query.id + "')";
       }
     }
     else if(req.query.type === "2"){    // 2 means by cohort
       SQLQuery = "SELECT CEILING(DATEDIFF(NOW(), ADDDATE(Start_Date, -243))/365) AS 'Year' FROM Student WHERE Student.fileID = '" + req.query.file + "'";
       if(req.query.id !== ""){
-        SQLQuery += " AND Student.Student_ID = '" + req.query.id + "'";
+        SQLQuery += " AND (Student.Student_ID LIKE '%" + req.query.id + "%' OR Student.Name = '" + req.query.id + "' OR Student.Start_Date = '" + req.query.id + "' OR Student.Program = '" + req.query.id + "')";
       }
     }
     if(SQLQuery === undefined){
