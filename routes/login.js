1d2dfcd89bba1bd27d9b4b01dc1b6bf572b7aaff
bjacobs <b.jacobs@unb.ca>
2021-11-02 12:45:39 -0300

    Made login page actually work

---
+++ routes/login.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const db = require('../models/index');
+const { Op } = require("sequelize");
+
+let router = express.Router();
+
+/** 
+ * API end point to get all the students in the student table and all those enrolled but not in the student table with a specific fileID, optional Student_ID
+ * Parameters:
+ *    file = The file ID (REQUIRED)
+ *    id = The Student ID (OPTIONAL, set to "" to skip)  
+*/
+router.get('/', async (req, res) => {
+  try {
+    const SQLQuery = "SELECT userID, username FROM Login WHERE username = '" + req.query.username + "' AND pswrd = SHA1('" + req.query.password + "')";
+    const resultTable = await db.sequelize.query(SQLQuery);
+    res.json(resultTable[0]);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+module.exports = router;
